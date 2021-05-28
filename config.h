9605ba430f66176a913c40916572e38443f845ba
ichigo-gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2021-05-28 19:52:10 +0530

    grid patch

--- config.h
+++ config.h
@@ -4,19 +4,20 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+  "JetBrains Mono:style=Medium:size=10",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#1b9fc6" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
+static unsigned int columns    = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight = 30;
 static unsigned int min_lineheight = 8;
 
 /*
