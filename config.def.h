5d948e2c822fac5235e6369ec31d0e9c17dedb0b
ichigo--gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2022-07-31 10:12:33 +0530

    change colours

--- config.def.h
+++ config.def.h
@@ -6,14 +6,15 @@ static int fuzzy = 1;  /* -F  option; if 0, dmenu doesn't use fuzzy matching  */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
     "FantasqueSansMono Nerd Font:style=Regular:size=12"};
-static const char *prompt = NULL; /* -p  option; prompt to the left of input field */
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
     /*                 fg         bg       */
-    [SchemeNorm]          = {"#bbbbbb", "#1a1b26"},
-    [SchemeSel]           = {"#eeeeee", "#449dab"},
-    [SchemeSelHighlight]  = {"#e0af68", "#449dab"},
-    [SchemeNormHighlight] = {"#e0af68", "#222222"},
-    [SchemeOut]           = {"#000000", "#00ffff"},
+    [SchemeNorm] = {"#1a1b26", "#eeeeee"},
+    [SchemeSel] = {"#eeeeee", "#449dab"},
+    [SchemeSelHighlight] = {"#e0af68", "#449dab"},
+    [SchemeNormHighlight] = {"#e0af68", "#eeeeee"},
+    [SchemeOut] = {"#000000", "#00ffff"},
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines = 0;
